a30a27f75414f2de014b250e74bb8ceb4891137b
greendoyle <wqz547397984@163.com>
2026-05-02 12:19:06 +0800

    增加防抖，减少触发频率，防止消息堆积。

--- app/src/main/java/com/greendoyle/aihelpmegetjob/permission/BossHrAccessibilityService.kt
+++ app/src/main/java/com/greendoyle/aihelpmegetjob/permission/BossHrAccessibilityService.kt
@@ -2,6 +2,7 @@ package com.greendoyle.aihelpmegetjob.permission
 
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
+import android.view.accessibility.AccessibilityNodeInfo
 import com.greendoyle.aihelpmegetjob.page_monitor.BossZhiPinPageMonitor
 import com.greendoyle.aihelpmegetjob.parser.BossZhiPinParser
 import com.greendoyle.aihelpmegetjob.utils.UiTreeTraverser
@@ -9,6 +10,27 @@ import com.greendoyle.aihelpmegetjob.utils.UiTreeTraverser
 class BossHrAccessibilityService : AccessibilityService() {
 
     private val nodeParser = BossZhiPinParser()
+
+    private val handler = android.os.Handler(android.os.Looper.getMainLooper())
+    private var scanRunnable: Runnable? = null
+
+    // 防抖触发扫描
+    private fun debounceScanCards(rootNode: AccessibilityNodeInfo?) {
+        // 先移除之前的任务，避免重复执行
+        scanRunnable?.let {
+            handler.removeCallbacks(it)
+            scanRunnable = null
+        }
+
+        // 新建延迟任务
+        val runnable = Runnable {
+            UiTreeTraverser.scanAllJobCards(rootNode)
+        }
+
+        scanRunnable = runnable
+        // 延迟 500ms 执行（可自己调 300~800）
+        handler.postDelayed(runnable, 500)
+    }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
         // 1. 获取当前界面根节点
@@ -22,7 +44,7 @@ class BossHrAccessibilityService : AccessibilityService() {
         if (isBossApp && isJobPage) {
 //            nodeParser.parse(rootNode)
             // UiTreeTraverser.traverseTree(rootNode)
-            UiTreeTraverser.scanAllJobCards(rootNode)
+            debounceScanCards(rootNode)
         }
     }
 
